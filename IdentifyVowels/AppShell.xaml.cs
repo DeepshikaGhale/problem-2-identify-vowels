ad5418810c824dc8c57c5bff3c553b1a94d7e449
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-05-29 17:33:08 -0400

    Upload identify vowels problem 2

---
+++ IdentifyVowels/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentifyVowels;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ IdentifyVowels/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace IdentifyVowels;
+
+public partial class MainPage : ContentPage
+{
+    //array of vowels
+	char[] arrayOfVowels = { 'a', 'e', 'i', 'o', 'u' };
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void onCheckBtnClicked(object sender, EventArgs e)
+	{
+        //stores user input
+		var userInput = entry.Text;
+
+        //validate if the user input is empty or not
+        if (userInput == null || userInput == "") {
+            errorMsg.Text = "Letter field should not be empty.";
+            errorMsg.IsVisible = true;
+            resultMsg.IsVisible = false;
+        }
+		else {
+            errorMsg.IsVisible = false;
+            //for-loop to go through every char in vowel array
+            foreach (char letter in arrayOfVowels)
+            {
+                //compare
+                if (userInput == letter.ToString())
+                {
+                    resultMsg.Text = "Yes, it is a vowel.";
+                    resultMsg.IsVisible = true;
+                    resultMsg.TextColor = Colors.Black;
+                    break;
+                }
+                else
+                {
+                    resultMsg.Text = "No, it is not a vowel.";
+                    resultMsg.IsVisible = true;
+                    resultMsg.TextColor = Colors.Red;
+                }
+            }
+        }
+			
+        SemanticScreenReader.Announce(CheckBtn.Text);
+	}
+}
+
+
